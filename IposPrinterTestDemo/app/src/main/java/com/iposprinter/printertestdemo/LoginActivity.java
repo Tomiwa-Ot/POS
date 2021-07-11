ecca49db3dec24df6c00d05722271831029027d4
Tropes-Ot <ot.server1@outlook.com>
2021-07-11 14:14:31 +0100

    made some changes

--- IposPrinterTestDemo/app/src/main/java/com/iposprinter/printertestdemo/LoginActivity.java
+++ IposPrinterTestDemo/app/src/main/java/com/iposprinter/printertestdemo/LoginActivity.java
@@ -3,18 +3,37 @@ package com.iposprinter.printertestdemo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.EditText;
+
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText email, passwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        email = (EditText) findViewById(R.id.edt_email);
+        passwd = (EditText) findViewById(R.id.edt_passwd);
     }
 
     public void login(View view){
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
+    }
+
+    public boolean loginDataValidate(){
+        if(!email.getText().toString().isEmpty() &&
+                Patterns.EMAIL_ADDRESS.matcher(email.getText().toString()).matches() &&
+                !passwd.getText().toString().isEmpty()
+        ){
+            return true;
+        }else {
+            return false;
+        }
     }
 }
